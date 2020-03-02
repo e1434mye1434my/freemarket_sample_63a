23a190cd1883b3f1b5bf7dabc9997de90fdbe604
nao085 <tkn.0805.a@icloud.com>
2020-03-02 22:42:27 +0900

    setting-routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'kari#index'
+  get 'mypages/index'
+  resources :mypages, only: [:index] do
+    collection do
+      get 'logout', to: 'mypages#logout'
+    end
+  end
 end
