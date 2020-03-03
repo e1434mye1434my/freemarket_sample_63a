4ee6264700ecd22e7fce91266439ba80852542d2
nao085 <tkn.0805.a@icloud.com>
2020-03-03 18:18:47 +0900

    move-to-modules-directory

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root to: 'kari#index'
   get 'orders/new'
   get 'products/show'
-  get 'mypages/index'
+  # get 'mypages/index'
   resources :mypages, only: [:index] do
     collection do
+      get '/',    to: 'mypages#index'
       get 'logout', to: 'mypages#logout'
     end
   end
