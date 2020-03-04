079e930abdbfa090ce1b635a23cadac689a4cb86
e1434mye1434my <59134132+e1434mye1434my@users.noreply.github.com>
2020-03-04 21:34:23 +0900

    3月4日　21時34分

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   get 'messages/index'
   get 'messages/upp'
   root to: "messages#index"
-  # resources :messages, only: [:edit, :update]
-  resources :messages, only: [:index, :edit, :update]
 end
