903f059f3f52ddad74431d02ca9aa256a527358c
masuzawa1 <57562777+masuzawa1@users.noreply.github.com>
2020-03-08 14:22:29 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'messages/index'
   get 'messages/upp'
-  root to: "messages#index"
+  root to: 'messages#index'
   get 'orders/new'
   get 'products/show'
 end
