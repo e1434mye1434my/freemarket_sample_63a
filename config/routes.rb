80b02d5316a8942465301300398725d1e808ad87
nao085 <tkn.0805.a@icloud.com>
2020-03-08 12:25:11 +0900

    setting-routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  # get 'cards/new'
+  # get 'cards/show'
   root to: 'kari#index'
   get 'orders/new'
   get 'products/show'
-  # get 'mypages/index'
+  get 'mypages/index'
+
+  resources :cards, only: [:new, :show] do
+    collection do
+      get 'show'
+    end
+  end
+
   resources :mypages, only: [:index] do
     collection do
-      get '/',    to: 'mypages#index'
+      # get '/', to: "mypages#index"
       get 'logout', to: 'mypages#logout'
     end
   end
