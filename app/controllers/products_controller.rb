28f5a87832a9cdad338676308740f6bfad6f16b3
nao085 <tkn.0805.a@icloud.com>
2020-03-17 20:10:38 +0900

    add-destroy-function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,35 @@
 class ProductsController < ApplicationController
+
+
+
+  
+  def new
+    @product = current_user.products.build
+  end
+
+  def create
+    @product = current_user.products.build(product_params)
+  end
+
   def show
-    
+    @product = Product.find(params[:id])
+  end
+  
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      redirect_to mypages_index_path, flash: { notice: "商品を削除しました"}
+    else
+      redirect_to product_path(params[:id])
+    end
+  end
+  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :stock, :condition, :size, :introduction, :sales_status)
   end
 end
+
+
