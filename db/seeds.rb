2cfb9bd24d2e53186b7a145c411899e9d7e98685
nao085 <tkn.0805.a@icloud.com>
2020-03-17 20:11:34 +0900

    add-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+  User.create!(
+    name: "テストマン#{n + 1}",
+    email: "test#{n + 1}@test.com",
+    password: 'foobar',
+    password_confirmation: 'foobar',
+    first_name: 'テスト',
+    last_name: 'マン',
+    firstFurigana: 'テスト',
+    last_Furigana: 'マン',
+    birth_year: '2000',
+    birth_month: '1',
+    birth_day: '1',
+    post_number: '0000000',
+    prefecture:  '大阪',
+    municipality: '淀川区',
+    house_number: '新高１丁目1-1',
+    tel: '00000000000',
+    icon: ''
+  )
+end
+
+Product.create!(
+  id: '1',
+  name: 'カメラ',
+  price: '15000',
+  stock: '1',
+  condition: '傷や汚れあり',
+  size: 'L',
+  introduction: '美品です',
+  sales_status: '販売中',
+  user_id: '1',
+  saler_id: "",
+  buyer_id: ""
+)
+
+Product.create!(
+  id: '2',
+  name: 'ハンバーガー',
+  price: '500',
+  stock: '1',
+  condition: '傷や汚れあり',
+  size: 'L',
+  introduction: '美品です',
+  sales_status: '販売中',
+  user_id: '1',
+  saler_id: "",
+  buyer_id: ""
+)
+
+Product.create!(
+  id: '3',
+  name: '掃除機',
+  price: '20000',
+  stock: '1',
+  condition: '傷や汚れあり',
+  size: 'L',
+  introduction: '美品です',
+  sales_status: '販売中',
+  user_id: '2',
+  saler_id: "",
+  buyer_id: ""
+)
