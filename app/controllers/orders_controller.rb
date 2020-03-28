adf19f16dfcc075828e2fad17c9271c3b463d44f
nao085 <tkn.0805.a@icloud.com>
2020-03-28 20:22:35 +0900

    add-order-function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,44 @@
 class OrdersController < ApplicationController
+
+  require 'payjp'
+
   def new
+    @product = Product.find(params[:id])
+    card = Card.where(user_id: current_user.id).first
+    if card.blank?
+      redirect_to controller: "cards", action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+      @card_brand = @default_card_information.brand
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
+    end
+  end
+
+  def pay
+    @product = Product.find(params[:id])
+    card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp::Charge.create(
+      amount: @product.price,
+      customer: card.customer_id,
+      currency: 'jpy'
+    )
+    @product.update(sales_status: 3, buyer_id: current_user.id)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,17 @@ Rails.application.routes.draw do
   root to: 'toppage#index'
   get 'messages/index'
   get 'messages/upp'
-  get 'orders/new'
-  get 'mypages/index'
   resources :products, only: [:show, :destroy]
 
-  resources :cards, only: [:new, :show] do
-    collection do
-      get 'show'
+  resource :cards, only: [:new, :create,:destroy, :show]
+
+  resources :order, only: %i[index] do
+    member do
+      get '/buy', to: 'orders#new'
+      post '/pay', to: 'orders#pay'
     end
   end
+
 
   resources :mypages, only: [:index] do
     collection do
