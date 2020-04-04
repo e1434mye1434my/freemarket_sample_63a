bd11c21ca86e3812fcb175c3588a8c3a9a7e0595
nao085 <tkn.0805.a@icloud.com>
2020-04-04 21:00:19 +0900

    add-done-action-and-change-sales-status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to controller: "cards", action: "new"
+      flash[:alert] = "購入にはクレジットカードの登録が必要です"
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
@@ -32,13 +33,47 @@ class OrdersController < ApplicationController
   def pay
     @product = Product.find(params[:id])
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-    Payjp::Charge.create(
-      amount: @product.price,
-      customer: card.customer_id,
-      currency: 'jpy'
-    )
-    @product.update(sales_status: 3, buyer_id: current_user.id)
-    redirect_to root_path
+    if @product.sales_status == "出品中"
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp::Charge.create(
+        amount: @product.price,
+        customer: card.customer_id,
+        currency: 'jpy'
+      )
+      @product.update(sales_status: 3, buyer_id: current_user.id)
+    else
+      raise
+    end
+    redirect_to action: 'done'
+    rescue => e
+      redirect_to  product_path(@product.id), alert: '商品の購入に失敗しました'
+  end
+
+  def done
+    path = Rails.application.routes.recognize_path(request.referer)
+    if path[:controller] == "orders" && path[:action] == "new"
+      @product = Product.find(params[:id])
+      card = Card.where(user_id: current_user.id).first
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(card.customer_id)
+        @default_card_information = customer.cards.retrieve(card.card_id)
+        @card_brand = @default_card_information.brand
+        case @card_brand
+        when "Visa"
+          @card_src = "visa.svg"
+        when "JCB"
+          @card_src = "jcb.svg"
+        when "MasterCard"
+          @card_src = "master-card.svg"
+        when "American Express"
+          @card_src = "american_express.svg"
+        when "Diners Club"
+          @card_src = "dinersclub.svg"
+        when "Discover"
+          @card_src = "discover.svg"
+        end
+      else
+        redirect_to root_path
+      end
   end
 end
