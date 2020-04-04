60d8eb8231dc8e7ea1b1cc964a166fba1b4fe73d
nao085 <tkn.0805.a@icloud.com>
2020-04-04 21:01:13 +0900

    fix-flash

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,18 +10,14 @@ class CardsController < ApplicationController
 
   def create
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-
-    # if card_params['payjp-token'].blank?
-    #   redirect_to action: "new"
-    # else
       customer = Payjp::Customer.create(
         card: params[:payjp_token],
         metadata: {user_id: current_user.id}
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
+        redirect_to action: "show"
         flash[:notice] = "追加しました。"
-        redirect_to action: "show", flash: { notice: "追加しました。"}
       else
         redirect_to action: "create"
       end
