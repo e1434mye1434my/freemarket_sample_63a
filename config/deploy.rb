a311bca5d52165b7da2712a6df8626c78e1ff0ba
nao085 <tkn.0805.a@icloud.com>
2020-04-05 14:32:46 +0900

    add redeploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/63_anight.pem']
+                  keys: ['~/.ssh/freemarketsample.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
