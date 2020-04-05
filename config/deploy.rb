cb2cda8ac21bc8108f44255c5a5ab7b6ab21ea60
nao085 <tkn.0805.a@icloud.com>
2020-04-05 16:59:36 +0900

    fix remote repo from ssh to https

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_63a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:e1434mye1434my/freemarket_sample_63a.git'
+set :repo_url,  'https://github.com/e1434mye1434my/freemarket_sample_63a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
