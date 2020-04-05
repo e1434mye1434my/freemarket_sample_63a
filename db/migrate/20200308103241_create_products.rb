3fe25ab4ade34127a81ef9c81e26eb32d686df87
tranquocnam <swordandhalberd07@gmail.com>
2020-04-05 17:04:18 +0900

    product_details_view

--- db/migrate/20200308103241_create_products.rb
+++ db/migrate/20200308103241_create_products.rb
@@ -4,15 +4,15 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :name,            null: false
       t.integer :stock          
       t.integer :price,          null: false
-      t.string :condition,       null: false
+      t.integer :condition_id,       null: false
       t.string :size            
       t.string :brand
       t.text :introduction,      null: false
       t.string :sales_status    
-      t.string :burden, null: false
+      t.integer :burden_id, null: false
       t.string :method          
       t.integer :prefecture_id,  null: false
-      t.string :estimated, null: false
+      t.integer :estimated_id, null: false
       t.integer :saler_id
       t.integer :buyer_id
       t.timestamps
