5d5a014584efa37bb1d4974344fcd49243837265
ninnin666 <you@example.com>
2020-04-09 15:07:14 +0900

    商品一覧 確認

--- db/migrate/20200308103241_create_products.rb
+++ db/migrate/20200308103241_create_products.rb
@@ -2,15 +2,15 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name,            null: false
-      t.integer :stock          
+      t.integer :stock
       t.integer :price,          null: false
       t.integer :condition_id,       null: false
-      t.string :size            
+      t.string :size
       t.string :brand
       t.text :introduction,      null: false
-      t.string :sales_status    
+      t.string :sales_status
       t.integer :burden_id, null: false
-      t.string :method          
+      t.string :method
       t.integer :prefecture_id,  null: false
       t.integer :estimated_id, null: false
       t.integer :saler_id
