235a0da45e9147320c568f69fbb0642d44d199ce
nao085 <tkn.0805.a@icloud.com>
2020-04-12 14:07:15 +0900

    add-comments-routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'messages/upp'
 
   resources :products, except: :index do
+    resources :comments, only: [:create, :destroy]
     member do
       get 'get_category_children', defaults: {format: 'json'}
       get 'get_category_grandchildren', defaults: {format: 'json'}
