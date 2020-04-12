1c45a331c3b758559df0d3e563b4c52a25e3a3cc
tranquocnam <swordandhalberd07@gmail.com>
2020-04-12 15:29:09 +0900

    flash_add

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,12 +3,9 @@ class MypagesController < ApplicationController
   end
 
   def show
-    
   end
 
   def likes
-    # user = User.find(params[:id])
-    # @product = user.products.all
   end
 
   def logout
