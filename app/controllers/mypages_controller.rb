03ba99ebcdc5588259eea375778c1097aeb582e8
nao085 <tkn.0805.a@icloud.com>
2020-04-15 23:01:39 +0900

    setting-controller

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -3,9 +3,21 @@ class MypagesController < ApplicationController
   end
 
   def show
+    @sold_products = Product.where(sales_status: 3, user_id: current_user.id)
   end
 
   def likes
+  end
+
+  def exhibiting
+    @exhibiting_products = Product.where(sales_status: 0, user_id: current_user.id)
+  end
+
+  def in_progress
+  end
+
+  def completed
+    @completed_products = Product.where(sales_status: 3, user_id: current_user.id)
   end
 
   def logout
